5567fb046ab277234db0375bc9a8f7d8fb9906f3
twmanley <tmanley1@yahoo.com>
2018-06-08 15:03:36 -0400

    first commit

---
+++ VirtualPetShelter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetShelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToEnter = "Welcome to the Virtual Pet Shelter ";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+    }
+}
